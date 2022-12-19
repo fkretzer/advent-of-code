7171e6aa3ca71c9eb3456e72a99dbf1f64a9d606
Fabian Kretzer <fabian.kretzer@innoq.com>
2022-12-19 14:37:34 +0100

    Optimize loading of input to use buffered Reader

--- src/main/kotlin/PuzzleInputReader.kt
+++ src/main/kotlin/PuzzleInputReader.kt
@@ -1,7 +1,9 @@
+import java.io.File
+
 class PuzzleInputReader {
     companion object {
         fun lines(filenameInResourceFolder: String): Sequence<String> =
-            this::class.java.getResource("3.input.txt").readText().split(System.lineSeparator()).asSequence()
+           File(this::class.java.getResource(filenameInResourceFolder).toURI()).bufferedReader().lineSequence()
     }
 }
 
